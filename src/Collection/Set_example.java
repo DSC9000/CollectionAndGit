73d3d65a783f7947e26e30b4ab75d63d7ce94f6b
Daniel S <devolution.sdc@gmail.com>
2023-09-06 20:22:49 -0500

    linkedHashSet ( insertion order )

--- src/Collection/Set_example.java
+++ src/Collection/Set_example.java
@@ -8,17 +8,21 @@ public class Set_example {
         num = num+1;
         System.out.println(num);
         String str ="abbbatzxxxxxfffffqqqq";
-
+        //arrayList
         List<String>list =new ArrayList<>();
         list.addAll(Arrays.asList(str.split("")));
         //[a, b, b, b, a, t, z, x, x, x, x, x, f, f, f, f, f, q, q, q, q]
         System.out.println(list);
+       //HashSet
         Set <String>setHash =new HashSet<>(); //takes out duplicates
         setHash.addAll(list);
         System.out.println(setHash);
         //[a, q, b, t, f, x, z]
+        //LinkedHash keeps insertion order
         Set<String> setLinked =new LinkedHashSet<>();
-        
+        setLinked.addAll(list);
+        System.out.println(setLinked);
+
 
     }
 }
