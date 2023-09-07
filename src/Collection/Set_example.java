f33d2d7f73efc74519cff5c2b0d0a97beb34e6a1
Daniel S <devolution.sdc@gmail.com>
2023-09-06 19:39:49 -0500

    str added to set class str contains duplicates

--- src/Collection/Set_example.java
+++ src/Collection/Set_example.java
@@ -1,9 +1,12 @@
 package Collection;
 
+import java.util.Set;
+
 public class Set_example {
     public static void main(String[] args) {
         int num =0;
         num = num+1;
         System.out.println(num);
+        String str ="abbbatzxxxxxfffffqqqq";
     }
 }
